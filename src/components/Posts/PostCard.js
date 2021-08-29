ff5aa73136a52db38a7906c47c8e68b60ecb996b
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-08-30 01:48:48 +0530

    implemented create post

--- src/components/Posts/PostCard.js
+++ src/components/Posts/PostCard.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { deletePost } from "../../actions/postAction";
-import { useDispatch } from "react-redux";
+// import { deletePost } from "../../actions/postAction";
+// import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 import EllipsisText from "react-ellipsis-text";
 const PostCard = ({ postItem }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   return (
     <div className="col-lg-3 col-md-6 mb-4">
       <div className="card custom-card">
@@ -14,18 +14,15 @@ const PostCard = ({ postItem }) => {
           alt={postItem.title}
           className="card-img-top"
         />
-        <Link
-          to={`/updatePost/${postItem.id}`}
-          className="btn btn-danger btn-edit"
-        >
+        {/* <Link to={`/updatePost/${postItem.id}`} className="btn  ">
           <span class="material-icons">edit</span>
         </Link>
         <button
-          className="btn btn-danger btn-delete"
+          className="btn"
           onClick={() => dispatch(deletePost(postItem.id))}
         >
           <span class="material-icons">delete_outline</span>
-        </button>
+        </button> */}
         <div className="card-body">
           <h6 className="text-secondary">{postItem.title}</h6>
           <EllipsisText text={postItem.body} length={"100"} />
--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./_navbar.scss";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
+import { GrClose } from "react-icons/gr";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -21,20 +22,26 @@ const Navbar = () => {
           onClick={handleToggle}
         >
           <ul>
-            <Link className="home_icon">Home</Link>
+            <Link to="/" className="home_icon">
+              Home
+            </Link>
             <li>
-              <a href="!#">Posts</a>
+              <a href="#">Posts</a>
             </li>
             <li>
-              <a href="!#">Contact</a>
+              <a href="#">Contact</a>
             </li>
           </ul>
           <Link to="/addPost" className="button">
             Add Posts
           </Link>
         </div>
-        <span className="toggle_bar">
+        <span className={toggle ? "toggle_bar close" : "toggle_bar open"}>
           <FaBars style={{ color: "white" }} onClick={handleToggle} />
+        </span>
+
+        <span className={toggle ? "close_btn open" : "close_btn"}>
+          <GrClose style={{ color: "red" }} onClick={handleToggle} />
         </span>
       </div>
     </nav>
